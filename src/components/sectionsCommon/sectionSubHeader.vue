3b4884f831b5a0feb4715c649a6975545e61763b
Alejandro <olartealejandro48@gmail.com>
2025-06-16 10:16:05 -0500

    feat(sectionSubHeader): improve title logic and styling
    - Update title logic to include 'Analytics' path and fix pluralization
    - Remove horizontal padding from container for better alignment
    - Conditionally render CTA section based on route

--- src/components/sectionsCommon/sectionSubHeader.vue
+++ src/components/sectionsCommon/sectionSubHeader.vue
@@ -1,21 +1,27 @@
  
  <template >
  <div class=" text-gray-800 ">
-      <div class="container mx-auto px-6 py-12">
+      <div class="container mx-auto py-12">
 
         <div 
           class="text-center mb-8"
            
          >
-          <h1   class="text-4xl md:text-6xl  mb-4 bg-clip-text text-transparent bg-gradient-to-r from-orange-600 to-purple-800 font-bold ">
-             {{ $route.path.includes('stocks') ?  'Stock': 'Dashboard '  }}
+          <h1 class="text-4xl md:text-6xl mb-4 bg-clip-text text-transparent bg-gradient-to-r from-orange-600 to-purple-800 font-bold">
+            {{ $route.path.includes('stocks') 
+                ? 'Stocks'
+                : $route.path.includes('analytics')
+                    ? 'Análisis de Mercado y Ratings'
+                    : 'Dashboard' }}
           </h1>
           <p class="text-xl  mb-8">
             Análisis inteligente *   Decisiones rentables *  Tiempo real
           </p>
           
           <!-- CTA Principal -->
-          <div class="flex flex-col sm:flex-row gap-4 justify-center items-center">
+    
+          <div  v-if="!$route.path.includes('analytics')" 
+            class="flex flex-col sm:flex-row gap-4 justify-center items-center">
             <router-link
              to="/stocks"
              v-if="!$route.path.includes('stocks')" 
