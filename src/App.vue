aed447f3b0ff8a7517be11aa3314b527542d3181
Alejandro <olartealejandro48@gmail.com>
2025-06-17 00:35:06 -0500

    feat(header): improve header styling and navigation layout
    - Add padding to header and adjust navigation items layout for better mobile responsiveness
    - Replace arrow indicator with star icon for active route
    - Uncomment and add "Análisis" route to navigation

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,11 @@
     <!-- Header -->
 
         <!-- bg-gradient-to-r   from-orange-500 via-orange-600 to-purple-600  -->
-<!--  -->
+<!--  sticky-->
     <header class="
         bg-gray-800
-        shadow-lg sticky top-0 z-50
-        text-emerald-800
+        shadow-lg  top-0 z-50
+        text-emerald-800 py-6
        ">
       <nav class="container mx-auto px-4 lg:px-6">
         <div class="flex flex-col sm:flex-row justify-between items-center py-4 
@@ -21,12 +21,13 @@
               >
                 <span class="text-3xl">📈</span>
                 <span class="hidden sm:inline">Stock Analyzer</span>
-                <span class="sm:hidden">SA</span>
+                <span class="sm:hidden">Stock </span>
               </router-link>
             </h1>
           </div>
            
-          <div class="flex items-center gap-2 sm:gap-4">
+          <div class="flex items-center mt-5 md:0 
+            flex-col sm:flex-row gap-2  text-sm  sm:gap-4">
             <router-link
               v-for="route in navigation"
               :key="route.name"
@@ -38,8 +39,9 @@
                   : 'text-white/80 hover:text-white hover:bg-white/10 border border-transparent hover:border-white/20',
               ]"
             >
-              <span class="hidden sm:inline"> {{$route.path !== route.path ? '->': ''}} {{ route.name }}</span>
-              <span class="sm:hidden">{{ route.name.charAt(0) }}</span>
+              <span class=" md:0 block w-full py-2"> 
+                  {{$route.path !== route.path ? '⭐': ''}} {{ route.name }}          </span>
+              <!-- <span class="sm:hidden">{{ route.name.charAt(0) }}</span> -->
             </router-link>
           </div>
         </div>
@@ -96,7 +98,7 @@
 const navigation = [
   { name: "Dashboard", path: "/" },
   { name: "Acciones", path: "/stocks" },
-  // { name: "Análisis", path: "/analytics" },
+  { name: "Análisis", path: "/analytics" },
   // { name: "Recomendaciones", path: "/recommendations" },
 ];
 </script>
